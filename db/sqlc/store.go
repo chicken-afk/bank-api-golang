0b0b7b1c583ff6d2810a6b1f1d569efae5dda329
andika <yuda.yokesen@gmail.com>
2023-02-07 13:04:20 +0700

    struct for transfer params and transfer result

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -21,7 +21,7 @@ func NewStore(db *sql.DB) *Store {
 }
 
 // ExecTx executes a function within a database transaction
-func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -37,4 +37,26 @@ func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) erro
 	}
 
 	return tx.Commit()
+}
+
+//TransferTx Pramaeters
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id`
+	ToAccountID   int64 `json:"to_account_id`
+	Amount        int64 `json:"amount`
+}
+
+//TransferTxResults is the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry`
+}
+
+// TransferTx performs a money transfer from one account to the other
+// It creates a transfer record, add account entries, and update account's balance
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+
 }
